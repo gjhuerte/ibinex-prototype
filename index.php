10ae8c947f4c0f30abed941107f8f704feeb4840
gjhuerte <gjhuerte@gmail.com>
2018-03-11 23:55:19 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Ibinex</title>
+  </head>
+  
+  <body>
+    <?php
+      echo 'Include me';
+    ?>
+    <script>
+      alert('hello world')
+    </script>
+  </body>
+</html>

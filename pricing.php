a09e9907ce1cc43226d6f9babd09e7cfd7ac3b18
rickycabugawan <rickycabugawan@gmail.com>
2018-03-12 02:33:31 -0400

    Added assets folder

---
+++ pricing.php
@@ -0,0 +1,27 @@
+<?php require 'template.php'; ?>
+
+<?php function styles_include(){ ?>
+
+<?php } ?>
+
+<?php function display_title(){ ?>
+Pricing
+<?php } ?>
+
+<?php function display_content(){ ?>
+<main>
+  <h1>Pricing</h1>
+  <p>Integration of the Ibinex turnkey cryptocurrency platform to exchange your way reguires</p>
+  <ul>
+    <li>$150,000 "reserve only, no payment" deposited in a local Escrow account of your choice for 180 days.</li>
+    <li>Your first 90 days of Ibinex are gratis while you build your book.</li>
+  </ul>
+  <h1>Our Fees</h1>
+  <p>0.1% flat-rate fee based on exchange volume with a minimum fee of $20,000 per month</p>
+  <p>Fees begin only at day 91</p>
+</main>
+<?php } ?>
+
+<?php function scripts_include(){ ?>
+
+<?php } ?>

0dd359c904686f593c1edb359f2fe0f6af114c52
macristinabsantos <macristinabsantos@gmail.com>
2018-03-12 06:54:18 -0400

    security page update

--- about.php
+++ about.php
@@ -5,11 +5,11 @@
 <?php } ?>
 
 <?php function display_title(){ ?>
-  About Us
+<b><i>Security</i></b>
 <?php } ?>
 
 <?php function display_content(){ ?>
-<div>lasdjalksjdalsjdlasjdlkas</div>
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
 <?php } ?>
 
 <?php function scripts_include(){ ?>
---
+++ security.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Ibinex-Security</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h2><b><i>Security</i></b></h2>
+  <h5>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et <br>
+    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut <br>
+    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehendrit in voluptate velit esse cillum <br>
+    dolore eu fugiat nulla pariatur. </h5>
+  
+    
+  </p>
+</div>
+  
+<div class="uk-section uk-section-default">
+    <div class="uk-container">
+
+       <h2><b>
+        Protection of Funds
+        </b></h2>
+        <br><br><br>
+
+        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
+      
+    The protection of your crypto-funds is of paramount importance to <br> us. For this reason, we take the ultimate lengths to ensure your <br>funds remain safe at all time.<br><br>
+    
+    As an indication of the high protective standards we hold, Ibinex <br>has been granted the status of fully-licensed, fully-compliant and as <br>a regulated custodian and account processor.<br><br>
+    
+    We are the first cryptocurrency exchange platform to hold this <br>mantle and offer exclusive payment solutions. As such, we must <br>demonstrate a consistent level of regulatory compliance in order to <br>retain this professional distinction, a title of which we are <br>immensely proud.   
+</div>
+  </div>
+    <div class="uk-section uk-section-muted">
+    <div class="uk-container">
+
+       <h2><b>
+        Encryption and Technologies
+        </b></h2>
+        <br><br><br>
+
+        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
+      <p>A successful cryptocurrency exchange suite is only as strong as its weakest link. Ibinex has left no encryption technology stone unturned, and has implemented the highest level of SSL security into the platform, complete with the most solid and secure technologies.</p>
+      <p>We ensure that no weak links exist to threaten the stability and core security of Ibinex for our customers. Threats are neutralised before they present themselves - we take no chances and anticipate potential breaches before they arise, entirely removing their potential before they ever eventuate.</p>
+    </div>
+    <div class="col-sm-5">
+      <h2><b>Information Security</b>
+      </h2>     
+      <p>Our platform is a 24/7 guarded operation, with all crypto-based funds held in cold-wallets, shielded by our own team of cyber security experts who work around the clock to ensure all funds are safe and untouchable by unauthorized seekers.</p>
+    </div>
+    <div class="col-sm-5">
+      <h2><b>Privacy</b>
+      </h2>     
+      <p>Security and confidentiality are of the utmost importance for our clients, and you can learn more about our completely transparent Regulations and Privacy Policy, to have complete peace of mind and confidence in Ibinex.</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>

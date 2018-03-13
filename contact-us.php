66b2a355950153d8763e58201b386098bacce3fa
philipjohn234 <philipjohn234@gmail.com>
2018-03-13 01:41:05 -0400

    Waw

--- contact-us.php
+++ contact-us.php
@@ -2,48 +2,85 @@
 
 <?php function styles_include(){ ?>
 <style>
-  .context{
-    background-image: url("https://images.pexels.com/photos/733852/pexels-photo-733852.jpeg?w=940&h=650&auto=compress&cs=tinysrgb");
+  .top-context{
+    background-image: url("https://images.pexels.com/photos/373076/pexels-photo-373076.jpeg?w=940&h=650&auto=compress&cs=tinysrgb");
     background-size: cover;
-  }
-  .context-inner-top{
-    margin-left: 10%;
-    max-width: 35%;
-    padding: 20% 10px 30px 0px;
+    background-position: center center;
+    height:100vh;
+    position:relative;
   }
   
-  h1{
-    font-weight: bold;
-  }
+ 
   
-  .middle{
-    width:100%;
-  }
-  
-  .context-middle{
-    padding: 10% 5% 2% 10%;
-    width: 35%;
-    display: inline-block;
-  }
-  
-  .form-container{
+  .top{
     position:absolute;
-    width:50%;
-    background-color:#b71c1c;
+    top:30%;
+    left:20%;
+    width:30%;
+    text-align:justify;
+  }
+  
+  .bottom-context{
+    position:relative;
     height: 100vh;
-    display:inline-block;
-    margin-top:-10px;
-    
+  }
+  
+  .bottom-left{
+    position:absolute;
+    height:inherit;
+    padding-top:10%;
+    padding-left: 20%;
+    text-align:justify;
+    width:25%;
+  }
+  
+  .bottom-right{
+    background-color: #b71c1c;
+    bottom:0;
+    left:50%;
+    width:50%;
+    height:105%;
+    position:absolute;
   }
   
   .white-form{
-    border: 1px solid white;
-    width: 50%;
-    position:absolute;
+    width: 40%;
+    margin: -70px 0 125px 80px;
     background-color:whitesmoke;
-    margin: -10% 10% 0 10%;
-    padding: 5%;
+    padding:5% 5% 5% 5%;
   }
+  
+  .label{
+    font-size: 0.8em;
+  }
+  
+  .form-textarea{
+    height:80%;
+    max-height: 225px;
+    -webkit-box-shadow: -1px 3px 8px -2px rgba(0,0,0,0.15);
+    -moz-box-shadow: -1px 3px 8px -2px rgba(0,0,0,0.15);
+    box-shadow: -1px 3px 8px -2px rgba(0,0,0,0.15);
+    border-radius:3px;
+  }
+  
+  .form-input{
+-webkit-box-shadow: -1px 3px 8px -2px rgba(0,0,0,0.15);
+-moz-box-shadow: -1px 3px 8px -2px rgba(0,0,0,0.15);
+box-shadow: -1px 3px 8px -2px rgba(0,0,0,0.15);
+     border-radius:3px;
+  }
+  
+  .form-button{
+    border-radius:3px;
+    background-color:#0D47A1;
+  }
+
+  
+
+  
+  
+  
+
   
 
  
@@ -55,13 +92,11 @@
 <?php } ?>
 
 <?php function display_content(){ ?>
-  <div class="uk-container-expand context"> <!-- Begin introduction -->
+<div class="top-context"> <!-- Begin introduction -->
     
-    <div class="context-inner-top">
-
-        <h1 class="uk-heading">Get Started</h1>
-        
-        <p class="intro-text">
+    <div class="top">
+      <h2><b>Getting Started</b></h2>
+      <p class="intro-text">
           We invite you to experience the Ibinex demo here, which will help you conceptualise
           the possibilities of personalisation for your own exchange.
           <br><br>
@@ -78,18 +113,13 @@
         <p class="intro-text">
           Please note that due to high demand, the current on-boarding timeframe is 4 months.
         </p>
-        
-      
     </div>
-    
-  </div> <!-- End of introduction -->
+</div> <!-- End of introduction -->
 
 
-
-<div class="middle">
-  <div class="context-middle">
-        <h1 class="uk-heading">Support</h1>
-        
+<div class="bottom-context"> <!-- Begin introduction -->
+        <div class="bottom-left">
+          <h2><b>Support</b></h2>
           <p class="intro-text">
           Contact and feedback are integral to the Ibinex way,
           enabling us to cater to your needs and requirements
@@ -104,37 +134,41 @@
           We will endeavour to respond to you as promptly as
           possible.
           </p>
-  </div>
+        </div>
   
-  <div class="form-container">
-      <div class="white-form">
-          <form>
-            
-              <div class="uk-margin">
-                  <label>Full Name</label>
-                  <input class="uk-input uk-form-width-large" type="text">
-              </div>
-              <div class="uk-margin">
-                  <label>Email Address</label>
-                  <input class="uk-input uk-form-width-large" type="text">
-              </div>
-              <div class="uk-margin">
-                  <label>Subject</label>
-                  <input class="uk-input uk-form-width-large" type="text">
-              </div>
-            
-              <div class="uk-margin">
-                  <label>Message</label>
-                  <textarea class="uk-input uk-form-width-large"></textarea>
-              </div>
+        <div class="bottom-right">
+          <div class="white-form">
+             
+            <form>
+                <div class="uk-margin">
+                    <label class="label">Full Name</label>
+                    <input class="uk-input uk-form-width-large form-input" spellcheck="false" type="text">
+                </div>
               
-              <button class="uk-button uk-button-primary">SEND</button>
-          </form>
-      </div>
-  </div>
-</div>
+                <div class="uk-margin">
+                    <label class="label">Email Address</label>
+                    <input class="uk-input uk-form-width-large form-input" spellcheck="false" type="text">
+                </div>
+              
+                <div class="uk-margin">
+                    <label class="label">Subject</label>
+                    <input class="uk-input uk-form-width-large form-input" spellcheck="false" type="text">
+                </div>
+              
+                <div class="uk-margin">
+                    <label class="label">Message</label>
+                    <textarea class="uk-input uk-form-width-large form-textarea" spellcheck="false" rows="5"></textarea>
+                </div>
+              
+                <div class="uk-margin">
+                    <button class="uk-button uk-button-primary form-button">SEND</button>
+                </div>
 
-
+            </form>
+            
+          </div>
+        </div>
+</div> <!-- End of introduction -->
 
 
 

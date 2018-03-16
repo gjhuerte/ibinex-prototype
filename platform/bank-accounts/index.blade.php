bec03d97cf812b79d02301aa8a8d5719dea899e4
stephene.ca <stephene.ca@ibinex.tech>
2018-03-16 05:05:41 -0400

    Changes made

--- platform/bank-accounts/index.blade.php
+++ platform/bank-accounts/index.blade.php
@@ -6,26 +6,46 @@
 
 <?php function styles_include(){ ?>
 <style>
-  input[type="text"] {
-  background: transparent;
-  border: none;
-  border-bottom: 1px solid #000000;
-  -webkit-box-shadow: none;
-  box-shadow: none;
-  border-radius: 0;
-}
+    input[type="text"] {
+    background: transparent;
+    border: none;
+    border-bottom: 1px solid #171b3e;
+    -webkit-box-shadow: none;
+    box-shadow: none;
+    border-radius: 0;
+  }
 
-input[type="text"]:focus {
-  -webkit-box-shadow: none;
-  box-shadow: none;
-}
+  input[type="text"]:focus {
+    -webkit-box-shadow: none;
+    box-shadow: none;
+  }
   
   .form-color {
     background-color: #0a0928;
   }
   
   .label1 {
-    color: white;  
+    color: white;
+    font-size: .7rem;
+  }
+  
+  .label2 {
+    color: white;
+    font-size: .5rem;
+  }
+  
+  .label3 {
+    color: #2f4b8a;
+    font-size: .5rem;
+  }
+  
+  .form-control1 {
+    border-radius: 1rem;
+    border: 1px solid #21234c;
+  }
+  
+  .formcontrol1:focus {
+    color: white;
   }
   
   .form-control:focus {
@@ -38,37 +58,40 @@ input[type="text"]:focus {
   
   .btn-color {
     background-color: #16d4ba;
-    color: #003030;
-    margin: 0 auto;
+    padding: 4% 15% 4% 15%;
+    font-size: .7rem;
+    font-weight: 900;
+    position: relative;
   }
   
   .form1-margin {
     
   }
   
-  input::placeholder {
+  .input-color {
     font-size: .7rem;
+    
 }
   
 </style>
 <?php } ?>
 
 <?php function display_content(){ ?>
-<form class="form-row col-md-4 form-color">
+<form class="form-row col-md-4 col-sm-4 form-color">
     <div class="form-row col-md-12 col-lg-12">
       <div class="form-group col-md-12 col-lg-12">
         <label class="label1" for="inputEmail4">ADD A BANK ACCOUNT</label>
       </div>
       <div class="form-group col-md-7 col-lg-7">
-        <label class="label1" for="inputState">BANK NAME</label>
-        <select id="inputState" class="form-control label1 bank-color">
+        <label class="label2" for="inputState">BANK NAME</label>
+        <select id="inputState" class="form-control form-control1 label1 bank-color">
           <option selected>SELECT BANK</option>
           <option>...</option>
         </select>
       </div>
       <div class="form-group col-md-5 col-lg-5">
-        <label class="label1" for="inputEmail4">CURRENCY</label>
-        <select id="inputState" class="form-control label1 bank-color">
+        <label class="label2" for="inputEmail4">CURRENCY</label>
+        <select id="inputState" class="form-control form-control1 label1 bank-color">
           <option selected>USD</option>
           <option>...</option>
         </select>
@@ -81,7 +104,7 @@ input[type="text"]:focus {
       <input type="text" class="form-control input-color" id="inputAddress2" placeholder="BANK ACCOUNT NUMBER">
     </div>
     <div class="form-group col-md-12">
-      <label for="inputCity">ACCOUNT HOLDER NAME</label>
+      <label class="label3" for="inputCity">ACCOUNT HOLDER NAME</label>
       <input type="text" class="form-control input-color" id="inputName" placeholder="KOUNIO ERIC">
     </div>
     <div class="form-group col-md-12">
